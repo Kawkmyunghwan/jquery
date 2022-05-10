d85e187219249bf6e0d300012b555ee0074ab36d
audghks1996 <audghks1996@naver.com>
2022-05-10 19:08:13 +0900

    22/05/10 보강

--- src/main/java/com/yedam/myserver/HomeController.java
+++ src/main/java/com/yedam/myserver/HomeController.java
@@ -1,6 +1,7 @@
 package com.yedam.myserver;
 
 import java.text.DateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
@@ -8,8 +9,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.yedam.myserver.users.vo.UserVO;
 
 /**
  * Handles requests for the application home page.
@@ -33,6 +40,26 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	@RequestMapping("list")
+	public void list(Model model) {
+		model.addAttribute("users", Arrays.asList("choi", "park"));
+	}
+	
+	@GetMapping("insert")
+	public void insert(@ModelAttribute("user") UserVO vo,
+						Model model) {
+		model.addAttribute("profile", vo);
+	}
+	
+	@PostMapping("insert")
+	public String insertProc(RedirectAttributes rattr) {
+		rattr.addAttribute("page", 2); //list 뒤에 쿼리문자열로 넘겨줌, list?page=2&keyword=park
+		rattr.addAttribute("keyword", "park");
+		rattr.addFlashAttribute("msg", "등록완료"); //한번만 보냄, 새로고침하면 지워짐. (쿼리스트링 x 내부적으로 보내줌)
+		return "redirect:list";
+		//list "요청(@RequestMapping)"으로 가서 처리한 후에 다시 list 페이지로 감
 	}
 	
 }
---
+++ src/main/java/com/yedam/myserver/excel/web/ExcelController.java
@@ -0,0 +1,24 @@
+package com.yedam.myserver.excel.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yedam.myserver.emp.mapper.EmployeeMapper;
+
+@Controller
+public class ExcelController {
+	@Autowired 
+	EmployeeMapper mapper;
+
+	@RequestMapping("/excel")
+	public String excel(Model model) {
+		String[] header = {"departmentId","departmentName","cnt"};
+		model.addAttribute("headers", header);
+		model.addAttribute("filename", "empList");
+		model.addAttribute("datas", mapper.findStat());
+		//List<VO> => list<map>
+		return "commonExcelView";
+	}
+}
